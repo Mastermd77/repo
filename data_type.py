a80d7379ab1b38820bfaaabaf6476b771d20cb68
ТвоёИмя <@email.com>
2026-01-22 14:42:53 +0500

    Краткое описание изменений

---
+++ data_type.py
@@ -0,0 +1,24 @@
+x = 5
+print(type(x))
+
+
+x = 1
+y = 35656222554887711
+z = -3255522
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+x = 3+5j
+y = 5j
+z = -5j
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+x = int(1)   # x will be 1
+y = int(2.8) # y will be 2
+z = int("3") # z will be 3
---
+++ hello.py
@@ -0,0 +1,7 @@
+#comments for this practice
+print("Hello, World!")
+
+if 5 > 2:
+  print("Five is greater than two!")
+  
+  print("I am", 35, "years old.")
